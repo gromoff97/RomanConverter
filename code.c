0c6d1c51b9ab2dec49eebb08af33ae160dcf5bc5
gromoff97 <gromoff97@mail.ru>
2019-10-20 15:09:12 +0700

    Roman code was copypasted.

--- code.c
+++ code.c
@@ -1,13 +1,9 @@
-
-
-
-
-
 // Function to convert decimal to Roman Numerals 
 int printRoman(int number) 
 { 
     int num[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000}; 
-    string sym[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"}; 
+    string sym[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+     
     int i=12;     
     while(number>0) 
     { 
