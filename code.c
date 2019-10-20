f842a610161f81c93eaff184269c5dff80ad969b
gromoff97 <gromoff97@mail.ru>
2019-10-20 16:32:58 +0700

    new condition + platform independent type for input.

--- code.c
+++ code.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #define LAST_INDEX 12
 
 int number_buffer[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
 char* symbol_buffer[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
 
-void print_roman(int input_number);
+void print_roman(int32_t input_number);
 
 /* Driver program */
 int main()
 {
-    print_roman(3549);
+    print_roman(0);
     return 0;
 }
 
 /* unction to convert decimal to Roman Numerals */
-void print_roman(int input_number)
+void print_roman(int32_t input_number)
 {
     size_t index = LAST_INDEX;
 
@@ -23,6 +24,12 @@ void print_roman(int input_number)
     {
       printf("0");
       return;
+    }
+
+    if (input_number < 0)
+    {
+      printf("-");
+      input_number = -input_number;
     }
 
     while (input_number > 0)
