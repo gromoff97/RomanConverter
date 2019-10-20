3c1087a1b5bd65cb8114bd2d191726994deaa2e7
gromoff97 <gromoff97@mail.ru>
2019-10-20 15:40:39 +0700

    Simplified

--- code.c
+++ code.c
@@ -5,16 +5,19 @@ void printRoman(int number)
 { 
     int num[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
     char* sym[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
-
     int i = 12;
+
     while (number > 0)
     { 
       int div = number / num[i];
-      number = number % num[i];
-      while (div--)
+
+      while (div != 0)
       {
         printf("%s", sym[i]);
+        div--;
       }
+
+      number = number % num[i];
       i--;
     }
 }
