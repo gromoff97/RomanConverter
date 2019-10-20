53b24dd46f02884b758ce209e3dc3e9227daa172
gromoff97 <gromoff97@mail.ru>
2019-10-20 15:40:56 +0700

    Condition for 0 was created.

--- code.c
+++ code.c
@@ -7,6 +7,12 @@ void printRoman(int number)
     char* sym[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
     int i = 12;
 
+    if (number == 0)
+    {
+      printf("0");
+      return;
+    }
+
     while (number > 0)
     { 
       int div = number / num[i];
