fa7f314285bce87c6b3380ef402a76c2e9a37554
gromoff97 <gromoff97@mail.ru>
2019-10-20 16:02:42 +0700

    refactored : function moved down to main.

--- code.c
+++ code.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
 
-int num[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
-char* sym[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+#define LAST_INDEX 12
+
+int number_buffer[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+char* symbol_buffer[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+
+void print_roman(int input_number);
+
+/* Driver program */
+int main()
+{
+    print_roman(3549);
+    return 0;
+}
 
 /* unction to convert decimal to Roman Numerals */
-void printRoman(int number)
+void print_roman(int input_number)
 {
-    int i = 12;
+    int index = LAST_INDEX;
 
-    if (number == 0)
+    if (input_number == 0)
     {
       printf("0");
       return;
     }
 
-    while (number > 0)
+    while (input_number > 0)
     { 
-      int div = number / num[i];
+      int div = input_number / number_buffer[index];
 
       while (div != 0)
       {
-        printf("%s", sym[i]);
+        printf("%s", symbol_buffer[index]);
         div--;
       }
 
-      number = number % num[i];
-      i--;
+      input_number = input_number % number_buffer[index];
+      index--;
     }
-}
-  
-/* Driver program */
-int main() 
-{
-    printRoman(3549);
-    return 0; 
 }
