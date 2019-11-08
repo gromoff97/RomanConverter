4f52f4f25f48caaadcc525af84dc1fd65bb61e72
gromoff97 <gromoff97@mail.ru>
2019-11-08 20:35:23 +0700

    Silly function for validating string for a number was created.

--- src/code.c
+++ src/code.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <ctype.h>
 
 #define LAST_INDEX 12
 
@@ -7,6 +9,7 @@ int number_buffer[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
 char* symbol_buffer[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
 
 void print_roman(int32_t input_number);
+int is_number(const char* input_buffer);
 
 /* Driver program */
 int main()
@@ -17,6 +20,32 @@ int main()
 
     print_roman(input);
     return 0;
+}
+
+int is_number(const char* input_buffer)
+{
+  size_t input_buffer_length;
+  size_t char_counter;
+  size_t start_index;
+
+  if (NULL == input_buffer)
+    return -1;
+
+  input_buffer_length = strlen(input_buffer);
+
+  if (0 == input_buffer_length)
+    return -2;
+
+  if ('-' == input_buffer[0] && 1 == input_buffer_length)
+    return -3;
+
+  start_index = ('-' == input_buffer[0]) ? 1 : 0;
+
+  for (char_counter = start_index; char_counter < input_buffer_length; char_counter++)
+    if (0 == isdigit(input_buffer[char_counter]))
+      return -4;
+
+  return 0;
 }
 
 /* function to convert decimal to Roman Numerals */
