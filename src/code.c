1406ed6fb6ef6bd671a5e496db5d570e94c396ce
gromoff97 <gromoff97@mail.ru>
2019-11-12 19:25:14 +0700

    new function; refactoring names; main-function has been rewritten.

--- src/code.c
+++ src/code.c
@@ -7,36 +7,48 @@
 #define LAST_INDEX 12
 #define INPUT_MAX_SIZE 32
 
-int number_buffer[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
-char* symbol_buffer[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+int numbers_buffer[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+char* symbols_buffer[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
 
+char* strtorom(const char* input_buffer);
 void print_roman(int32_t input_number);
 int is_number(const char* input_buffer);
 
 /* Driver program */
 int main()
 {
+    /* Here is the place where input is stored. */
     char input[INPUT_MAX_SIZE];
-    int64_t input_int;
 
+    char* result;
+
+    /* Simplest UI offering to enter something. */
     puts("Please enter value:");
+
+    /* Reading from keyboard (strictly speaking, from standard input). */
     if (NULL == fgets(input, INPUT_MAX_SIZE, stdin))
     {
       fprintf(stderr, "Error occured while reading input.\n");
       return -1;
     }
 
-    /* removing trailing newline */
+    /* Removing trailing newline. */
     input[strcspn(input, "\n")] = '\0';
 
-    if (0 != is_number(input))
+    /* Calling function which converts char */
+    result = strtorom(input);
+
+    if (NULL == result)
     {
-      fprintf(stderr, "Error occured while validating: '%s' is not a number.\n", input);
+      fprintf(stderr, "Error occured while converting.\n");
       return -2;
     }
 
-    input_int = strtol(input, NULL, 10);
-    print_roman(input_int);
+    printf("%s", result);
+
+    /* Should free because strtorom allocates memory for result */
+    free(result);
+
     return 0;
 }
 
@@ -66,34 +78,66 @@ int is_number(const char* input_buffer)
   return 0;
 }
 
-/* function to convert decimal to Roman Numerals */
-void print_roman(int32_t input_number)
+/* Function for converting string to Roman Numerals. */
+char* strtorom(const char* input_buffer)
 {
-    size_t index = LAST_INDEX;
+  /* Represents pointer to buffer which possibly will contain string in Roman Numerals. */
+  char* result_buffer;
 
-    if (input_number == 0)
+  /* Represents the offset in result buffer. */
+  size_t result_counter;
+
+  /* Represents the integer value whis is got from function's argument */
+  int64_t input_number;
+
+  /* Represents the offset in buffers describing relation between decimal numbers and roman numbers. */
+  size_t numbers_index;
+
+  /* Validating function's argument. */
+  if (0 != is_number(input_buffer))
+    return NULL;
+
+  /* Converting char buffer to integer. */
+  input_number = strtol(input_buffer, NULL, 10);
+
+  /* Allocating enough memory for result. */
+  result_buffer = malloc(sizeof(char) * 1024);
+
+  /* If converted number equals to 0 then there is no point to continue main loop. */
+  if (input_number == 0)
+  {
+    result_buffer = "0\0";
+    return result_buffer;
+  }
+
+  /* If converted number is less than zero then add 'minus'-symbol to start of result buffer before main loop. */
+  if (input_number < 0)
+  {
+    result_buffer[0] = '-';
+    input_number = -input_number;
+  }
+
+  /* Assigning to last element's index because this is how main loop works.*/
+  numbers_index = LAST_INDEX;
+
+  /* If 'minus'-symbol exists in result buffer then set offset to 1. Otherwise set to 0 */
+  result_counter = (result_buffer[0] == '-') ? 1 : 0;
+
+  /* Main loop. */
+  while (input_number > 0)
+  {
+    int64_t div = input_number / numbers_buffer[numbers_index];
+
+    while (div != 0)
     {
-      printf("0");
-      return;
+      strcat(&result_buffer[result_counter], symbols_buffer[numbers_index]);
+      result_counter += strlen(symbols_buffer[numbers_index]);
+      div--;
     }
 
-    if (input_number < 0)
-    {
-      printf("-");
-      input_number = -input_number;
-    }
+    input_number = input_number % numbers_buffer[numbers_index];
+    numbers_index--;
+  }
 
-    while (input_number > 0)
-    { 
-      int div = input_number / number_buffer[index];
-
-      while (div != 0)
-      {
-        printf("%s", symbol_buffer[index]);
-        div--;
-      }
-
-      input_number = input_number % number_buffer[index];
-      index--;
-    }
+  return result_buffer;
 }
